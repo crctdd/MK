40ef453dfc027ea3e63187c6da888a4c18c5b468
TS <105903283+crctdd@users.noreply.github.com>
2025-03-13 13:48:31 +0800

    Add files via upload

---
+++ Rewrite/MaNiuBi.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/crctdd/MK/refs/heads/main/Profiles/MaNiuBi.js
+
+[mitm] 
+hostname = api.dragonlongzhu.cn
