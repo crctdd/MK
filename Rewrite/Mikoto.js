dde57151efb36689c6cec58479bbf71cf4dab8b5
TS <105903283+crctdd@users.noreply.github.com>
2025-03-27 17:53:45 +0800

    更新 Mikoto.js

--- Rewrite/Mikoto.js
+++ Rewrite/Mikoto.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/crctdd/QuantumultX/refs/heads/main/Rewrite/Mikoto1.js
+^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/crctdd/QuantumultX/refs/heads/main/Rewrite/Mikoto.js
 
 [mitm] 
 hostname = api.dragonlongzhu.cn
